a8b6a6dcc4217b2596bd782e9ff87e76fbae9ec4
scarnell <scarnell@ufl.edu>
2016-01-31 00:01:28 -0500

    add scoring: enemies closer to player are worth more points. enemies can also grow larger as they get closer to player (for more points). coded to end at 250 pts.

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using CompleteProject;
+
+public class EnemyManager : MonoBehaviour
+{
+	public GameObject[] spawnedEnemies;
+    
+    public GameObject enemy;
+    public float spawnTime = 5f;
+	public float spawnTimeMid = 5f;
+    public Transform[] spawnPoints;
+
+	int spawnCount = 0;
+
+
+    void Start ()
+    {
+        Invoke ("Spawn", 3);
+
+    }
+
+	void Update(){
+	}
+
+    void Spawn ()
+    {
+		if (ScoreManager.score < ScoreManager.endScore) {
+			int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+			spawnTime = (float)Random.Range (10, 12);
+
+			GameObject instance = (GameObject)Instantiate (enemy, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+
+
+			spawnCount++;
+
+			Invoke ("Spawn", spawnTime);
+		}
+
+
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using CompleteProject;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+	public static int endScore = 250;
+	GameObject[] enemies;
+
+    Text text;
+
+
+    void Awake ()
+    {
+        text = GetComponent <Text> ();
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+		
+        
+		if (score >= endScore) {
+			
+			text.text = "You won!";
+
+		} else {
+			text.text = "Score: " + score;
+		}
+		
+    }
+}
